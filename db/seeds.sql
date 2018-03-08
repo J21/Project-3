2b44b4ce518f026d86a0a4223a2f2edb94e3e2b5
J <J21>
2018-03-07 19:08:02 -0600

    Cleared

--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('hamburger', TRUE);
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', TRUE);
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('veggie burger', FALSE);
